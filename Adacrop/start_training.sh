49f6def2eaa4c738a791fb290afc9780f884ee04
ZyanNo1 <lzy2764023336@gmail.com>
2026-05-18 11:43:01 +0800

    fix: remove accidentally committed car.jpg and start_training.sh

--- Adacrop/start_training.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=/usr/local/lib/python3.8/dist-packages/torch/lib:$LD_LIBRARY_PATH
-export PYTHONUNBUFFERED=1
-
-cd /ai/lzy/LiveCompose
-python -u Adacrop/src/trainer.py
